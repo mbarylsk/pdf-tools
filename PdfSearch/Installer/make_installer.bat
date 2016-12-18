3ea42a896f5bdb6a3f6ffa9eb49ecc07fe487e07
Marcin <marcin.barylski@tas-moto.org>
2016-12-18 12:41:19 +0100

    Introducing installer creation to post-build event

--- PdfSearch/Installer/make_installer.bat
+++ PdfSearch/Installer/make_installer.bat
@@ -1,24 +1,27 @@
+set my_path="D:\Sources\Github\pdf-tools\PdfSearch"
+cd %my_path%
+
 REM Delete previous artifacts
-DEL /F /Q ..\sources.zip
-DEL /F /Q ..\..\WebPage\download\pdfsearch.msi
-DEL /F /Q ..\..\WebPage\download\pdfsearch64.msi
+DEL /F /Q sources.zip
+DEL /F /Q ..\WebPage\download\pdfsearch.msi
+DEL /F /Q ..\WebPage\download\pdfsearch64.msi
 SET PATH=C:\Program Files (x86)\7-Zip;C:\Program Files (x86)\WiX Toolset v3.7\bin\;%PATH%
 
 REM Create sources.zip
-RMDIR /S /Q ..\PdfSearch\bin\
-RMDIR /S /Q ..\PdfSearch\obj\
-7z.exe a ..\sources.zip ..\PdfSearch
+RMDIR /S /Q PdfSearch\bin\
+RMDIR /S /Q PdfSearch\obj\
+7z.exe a sources.zip PdfSearch
 
 REM Create installer - 32bit
-candle pdfsearch.wxs
-light pdfsearch.wixobj
+candle %my_path%\Installer\pdfsearch.wxs
+light %my_path%\pdfsearch.wixobj
 
 REM Create installer - 64bit
-candle pdfsearch64.wxs
-light pdfsearch64.wixobj
+candle %my_path%\Installer\pdfsearch64.wxs
+light %my_path%\pdfsearch64.wixobj
 
 REM Deploy installer
-MOVE pdfsearch.msi ..\..\WebPage\download
-MOVE pdfsearch64.msi ..\..\WebPage\download
+MOVE pdfsearch.msi ..\WebPage\download
+MOVE pdfsearch64.msi ..\WebPage\download
 
 PAUSE
